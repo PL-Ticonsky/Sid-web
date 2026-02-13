6757ac13894b7ba2be936bfbf4ae3ea01a2d53d8
Ticonsky <josueurregolopez02@gmail.com>
2026-02-13 16:59:47 -0500

    deploy: github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://pl-ticonsky.github.io",
+  base: "/sid-web/",
+  output: "static",
 });
+
